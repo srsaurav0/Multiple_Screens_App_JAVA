79f65dbe6061882cf74b66185fb75cb4c7beb527
srsaurav0 <srsaurav0@gmail.com>
2022-05-16 22:10:27 +0600

    New Class added.

---
+++ app/src/main/java/Created/Created.java
@@ -0,0 +1,39 @@
+package Created;
+
+public class Created {
+    int a;
+    String b;
+    double c;
+
+    public Created(int a, String b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public String getB() {
+        return b;
+    }
+
+    public double getC() {
+        return c;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public void setB(String b) {
+        this.b = b;
+    }
+
+    public void setC(double c) {
+        this.c = c;
+    }
+}
+
+
